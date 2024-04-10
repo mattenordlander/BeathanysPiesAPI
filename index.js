ab694aca10d1613dcfed1a3f97ae0515f305ee6c
mattenordlander <mattsson.nord@gmail.com>
2024-04-10 12:49:24 +0200

    Retrieve and search data rest meth (#2)
    * Return array of data from REST API
    
    * Create module for pie data
    
    * Wrape JSON envelope around return data
    
    * Move to folder
    
    * Retrive data from repo folder
    
    * Read pie data from file
    
    * Get a single piece of data
    
    * Get search function from pieRepo
    
    * Create search argument

--- index.js
+++ index.js
@@ -1,14 +1,68 @@
 // Bring in the express server and create application
 let express = require('express');
 let app = express();
-
+let pieRepo = require('./repos/pieRepo')
 // use the express Router obejct
 let router = express.Router();
-
 // Create GET to return a list of all  pies
 router.get('/', function (req, res, next){
-    res.send("Apple");
+
+   pieRepo.get(function(data){
+     // you should always return status()
+     res.status(200).json({
+        "status":200,
+        "statusText":"OK",
+        "message": "All pies retrived",
+        "data":data
+    });
+   },function(err){
+    next(err);
+   });
 });
+// Create GET/search?id=n&name=str to search for pies by 'id' and/or 'name'
+router.get('/search', function(req,res,next){
+    let searchObject = {
+        "id": req.query.id,
+        "name": req.query.name
+    };
+
+    pieRepo.search(searchObject, function (data){
+        res.status(200).json({
+            "status":200,
+            "statusText":"OK",
+            "message": "All pies retrieved",
+            "data": data
+        });
+    }, function (err){
+        next(err)
+    });
+});
+// Create GET/id to return a single pie
+router.get('/:id', function(req,res,next){
+    pieRepo.getById(req.params.id, function(data){
+        if(data){
+            res.status(200).json({
+                "status":200,
+                "statusText":"OK",
+                "message":"Single pie retrieved",
+                "data":data
+            });
+        }
+        else{
+            res.status(404).json({
+                "status":404,
+                "statusText":"Not Found",
+                "message": "The pie '"+ req.params.id +"'could not be found",
+                "error": {
+                    "code": "NOT_FOUND",
+                    "message": "The pie '"+ req.params.id +"'could not be found"
+                }
+            });
+        }
+    },function(err){
+        next(err);
+    });
+})
 
 // Configure router so all routes are prefixed with /api/v1
 app.use('/api/',router);
