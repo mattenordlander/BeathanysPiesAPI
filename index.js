4157307451a850cb6cc88e023a473062d81133f4
Mathias Nordlander <mattsson.nord@gmail.com>
2024-04-15 19:12:52 +0200

    Create put/update function

--- index.js
+++ index.js
@@ -81,6 +81,34 @@ router.post('/', function(req,res,next){
     });
 })
 
+router.put('/:id', function(req,res,next){
+    pieRepo.getById(req.params.id, function (data){
+        if(data){
+            // Attempt to update the data
+            pieRepo.update(req.body, req.params.id, function (data){
+                res.status(200).json({
+                    "status":200,
+                    "statusText":"OK",
+                    "message": "pie'"+ req.params.id +"' updated",
+                    "data":data
+                });
+            });
+        }
+        else{
+            res.status(404).json({
+                "status":404,
+                "statusText":"Not found",
+                "message": "the pie'"+ req.params.id +"' could not be found.",
+                "error":{
+                    "code":"NOT_FOUND",
+                    "message": "the pie'"+ req.params.id +"' could not be found."
+                }
+            });
+        }
+    },function(err){
+        next(err);
+    });
+})
 // Configure router so all routes are prefixed with /api/v1
 app.use('/api/',router);
 
