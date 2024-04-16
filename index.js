766edba6d0afa37e1fccd9c5cbca3c6948fb1cd2
mattenordlander <mattsson.nord@gmail.com>
2024-04-16 14:34:39 +0200

    Methods to modify data (#3)
    * Add POST function
    
    * Create put/update function
    
    * Change data with Postman
    
    * Create add and delete arguments
    
    * Create delete argument for repo object

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ let app = express();
 let pieRepo = require('./repos/pieRepo')
 // use the express Router obejct
 let router = express.Router();
+
+// Configure middleware to support JSON data parsing if request object
+app.use(express.json());
+
 // Create GET to return a list of all  pies
 router.get('/', function (req, res, next){
 
@@ -62,8 +66,107 @@ router.get('/:id', function(req,res,next){
     },function(err){
         next(err);
     });
+});
+
+router.post('/', function(req,res,next){
+    pieRepo.insert(req.body, function(data){
+        res.status(201).json({
+            "status": 201,
+            "statusText":"Created",
+            "message": "New Pie Added",
+            "data":data
+        });
+    }, function(err){
+        next(err)
+    });
 })
 
+router.put('/:id', function(req,res,next){
+    pieRepo.getById(req.params.id, function (data){
+        if(data){
+            // Attempt to update the data
+            pieRepo.update(req.body, req.params.id, function (data){
+                res.status(200).json({
+                    "status":200,
+                    "statusText":"OK",
+                    "message": "pie '"+ req.params.id +"' updated",
+                    "data":data
+                });
+            });
+        }
+        else{
+            res.status(404).json({
+                "status":404,
+                "statusText":"Not found",
+                "message": "the pie '"+ req.params.id +"' could not be found.",
+                "error":{
+                    "code":"NOT_FOUND",
+                    "message": "the pie '"+ req.params.id +"' could not be found."
+                }
+            });
+        }
+    },function(err){
+        next(err);
+    });
+})
+
+router.delete('/:id', function(req,res,next){
+    pieRepo.getById(req.params.id, function(data){
+        if(data){
+            // Attempt to delete the data
+            pieRepo.delete(req.params.id, function(data){
+                res.status(200).json({
+                    "status":200,
+                    "statusText":"OK",
+                    "message": "The pie '"+req.params.id+"'is deleted",
+                    "data": "The pie '"+req.params.id+"'is deleted"
+                })
+            })
+        }
+        else{
+            res.status(404).json({
+                "status":404,
+                "statusText":"Not found",
+                "message": "the pie '"+ req.params.id +"' could not be found.",
+                "error":{
+                    "code":"NOT_FOUND",
+                    "message": "the pie '"+ req.params.id +"' could not be found."
+                }
+            });
+        }
+    }, function(err){
+        next(err);
+    });
+})
+
+router.patch('/:id', function(req,res,next){
+    pieRepo.getById(req.params.id, function (data){
+        if(data){
+            // Attempt to update the data
+            pieRepo.update(req.body, req.params.id, function (data){
+                res.status(200).json({
+                    "status":200,
+                    "statusText":"OK",
+                    "message": "pie '"+ req.params.id +"' patched",
+                    "data":data
+                });
+            });
+        }
+        else{
+            res.status(404).json({
+                "status":404,
+                "statusText":"Not found",
+                "message": "the pie'"+ req.params.id +"' could not be found.",
+                "error":{
+                    "code":"NOT_FOUND",
+                    "message": "the pie'"+ req.params.id +"' could not be found."
+                }
+            });
+        }
+    },function(err){
+        next(err);
+    });
+})
 // Configure router so all routes are prefixed with /api/v1
 app.use('/api/',router);
 
